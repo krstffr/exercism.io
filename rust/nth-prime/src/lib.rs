c9642ab2e3ce2c0a69f9891f0245978f41d1f51b
Kristoffer Klintberg <krstffr@gmail.com>
2020-06-09 23:39:50 +0200

    works, but is SUPER bad and inefficent

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,24 +1,27 @@
-
-// Wow this is getting bad!
-fn is_prime(n: u32) -> bool {
-    let mut found = false;
-    for x in 3..n {
-        if x == n { break } else { () }
-        found = 
-    }
-    found
-}
-
 pub fn nth(n: u32) -> u32 {
     match n {
         0 => 2,
         1 => 3,
         n => {
-            let mut res = 0;
-            for x in 2..n {
-                res = x
-            }
-            res
+            let mut next_prime_n = 2;
+            let mut x = 4;
+            // LOL this is shit!!
+            // Should try some non-naive algorithm, read this:
+            // https://www.programminglogic.com/testing-if-a-number-is-prime-efficiently/
+            let value = loop {
+                let mut is_prime = true;
+                for y in 2..x {
+                    is_prime = is_prime && x % y != 0
+                }
+                if is_prime {
+                    next_prime_n += 1;
+                    if next_prime_n == (n + 1) {
+                        break x;
+                    }
+                }
+                x += 1;
+            };
+            value
         }
     }
 }
