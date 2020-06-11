c50b3dff8a270341e2fd7e4a425bac3f234d83af
Kristoffer Klintberg <krstffr@gmail.com>
2020-06-11 16:37:19 +0200

    a more FP working, no mut!

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,10 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let num_str = format!("{}", num);
-    let len = num_str.len().to_string().parse().unwrap();
-    let mut sum = 0;
-    for x in num_str.chars() {
-        let n = x.to_string().parse::<u32>().unwrap();
-        sum += u32::pow(n, len);
-    }
+    let str_len = num_str.len().to_string().parse().unwrap();
+    let sum: u32 = num_str
+        .chars()
+        .map(|x| u32::pow(x.to_string().parse().unwrap(), str_len))
+        .sum();
     sum == num
 }
