f2610ce48a0e4f3de0540b9ebb9168ca06804811
Kristoffer Klintberg <krstffr@gmail.com>
2020-06-11 16:38:57 +0200

    nicer string length -> u32

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let num_str = format!("{}", num);
-    let str_len = num_str.len().to_string().parse().unwrap();
+    let str_len = num_str.len() as u32;
     let sum: u32 = num_str
         .chars()
         .map(|x| u32::pow(x.to_string().parse().unwrap(), str_len))
